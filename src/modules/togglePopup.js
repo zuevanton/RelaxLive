8054f8b0391c9a659d3cf4ba618395e5e1c8f7a3
Anton Zuev <wnfgnqpw@gmail.com>
2020-03-13 14:05:29 +0300

    консультация попап + правки

--- src/modules/togglePopup.js
+++ src/modules/togglePopup.js
@@ -27,7 +27,7 @@
       }
       document.addEventListener('click', (e) => {
         const target = e.target;
-        if(!target.closest('.popup') || target.closest('.menu-link')) return;
+        if(!target.closest('.popup') || target.closest('.menu-link') || target.closest('.link-privacy')) return;
         if(target.closest('.close')){
           this.closePopup();
         } else if (!target.closest(this.popupContent) && this.isOpen){
