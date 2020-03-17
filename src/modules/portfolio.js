5295bc5bb4b1e5db937153e43e5d1dddf0aa502c
Anton Zuev <wnfgnqpw@gmail.com>
2020-03-17 20:31:06 +0300

    слайдер портфолио

--- src/modules/portfolio.js
+++ src/modules/portfolio.js
@@ -1,9 +1,10 @@
 import sliderCarousel from "./sliderCarousel";
+import Popup from "./togglePopup";
 
 const portfolio = () => {
-
+  let portfolioSlider;
   if(document.documentElement.clientWidth > 576) {
-    const portfolioSlider = new sliderCarousel({
+    portfolioSlider = new sliderCarousel({
       main: '.portfolio-slider-wrap',
       wrap: '.portfolio-slider',
       prev: '#portfolio-arrow_left',
@@ -36,7 +37,7 @@ const portfolio = () => {
     });
   }
   else {
-    const portfolioSlider = new sliderCarousel({
+    portfolioSlider = new sliderCarousel({
       main: '.portfolio-slider-wrap',
       wrap: '.portfolio-slider-mobile',
       prev: '#portfolio-arrow-mobile_left',
@@ -47,7 +48,73 @@ const portfolio = () => {
     });
     portfolioSlider.init();
     portfolioSlider.setCounter();
+
   }
+
+  const showText = (textNumber) => {
+    texts.forEach((item, i) => {
+      if(i === textNumber){
+        item.style.display = 'block';
+      } else {
+        item.style.display = '';
+      }
+    });
+  };
+
+  const prev = document.getElementById('popup_portfolio_left'),
+        next = document.getElementById('popup_portfolio_right'),
+        texts = document.querySelectorAll('.popup-portfolio-text');
+  const syncSliders = (i) => {
+    if(document.documentElement.clientWidth < 577) {
+      portfolioSlider.setCounter(i + 1);
+      portfolioSlider.showSlide(i);
+    }
+  };
+  next.addEventListener('click', () => {
+    syncSliders(sliderPopup.slideNum);
+    if(sliderPopup.slideNum >= 10){
+      showText(0);
+      return;
+    }
+    showText(sliderPopup.slideNum);
+  });
+  prev.addEventListener('click', () => {
+    syncSliders(sliderPopup.slideNum - 2);
+    if(sliderPopup.slideNum < 2){
+      showText(0);
+      return;
+    }
+    showText(sliderPopup.slideNum - 2);
+  });
+
+  const portfolioPopup = new Popup({
+    popup: '.popup-portfolio',
+    popupBtn: '.portfolio-slider__slide-frame',
+    popupContent: '.popup-dialog-portfolio'
+  });
+  portfolioPopup.init();
+
+  const sliderPopup = new sliderCarousel({
+    main: '.popup-portfolio-slider-wrap',
+    wrap: '.popup-portfolio-slider',
+    prev: '#popup_portfolio_left',
+    next: '#popup_portfolio_right',
+    slideToShow: 1,
+    infinity: true,
+    countered: true
+  });
+  sliderPopup.init();
+  sliderPopup.setCounter();
+
+  const slidesFrames = document.querySelectorAll('.portfolio-slider__slide-frame');
+  slidesFrames.forEach((item, i) => {
+    item.addEventListener('click', () => {
+      i = i >= 10 ? i - 10 : i;
+      sliderPopup.setCounter(i+1);
+      sliderPopup.showSlide(i);
+      showText(i);
+    });
+  });
 };
 
 export default portfolio;
